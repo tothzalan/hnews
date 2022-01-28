d6f33f9baaa2541ab9312607bff9f1f49f20f3c2
tothzalan <toth.zalan03@gmail.com>
2022-01-28 10:39:59 +0100

    Search users

--- src/App.svelte
+++ src/App.svelte
@@ -14,9 +14,11 @@
 <Router {url}>
 	<nav class="navBar">
 		<Link to="/">Home</Link>
+		<Link to="/user">User</Link>
 	</nav>
 	<main>
 		<Route path="thread/:id" let:params><Thread id={+params.id}></Thread></Route>
+		<Route path="user"><User></User></Route>
 		<Route path="user/:username" let:params><User username={params.username}></User></Route>
 		<Route path="/"><Home /></Route>
 	</main>
--- src/routes/User.svelte
+++ src/routes/User.svelte
@@ -3,20 +3,30 @@
     import User from "../components/User.svelte";
 
     export let username: string;
+    let usernameInput: string;
 
     let userData: JSON;
 
     onMount(async () => {
-        const resGetUser: any = await fetch(`https://hacker-news.firebaseio.com/v0/user/${username}.json`);
-        userData = await resGetUser.json();
+        if(username) {
+            await getUserData(username);
+        }
     });
+
+    async function getUserData(name: string) {
+        const resGetUser: any = await fetch(`https://hacker-news.firebaseio.com/v0/user/${name}.json`);
+        userData = await resGetUser.json();
+    }
 
 </script>
 
+
 <div class="container">
+    {#if !username}
+        <input bind:value={usernameInput}>
+        <button on:click|preventDefault={()=> { getUserData(usernameInput)}}>Get user</button>
+    {/if}
     {#if userData}
         <User userData={userData} />
-    {:else}
-        <p>Loading ...</p>
     {/if}
 </div>
